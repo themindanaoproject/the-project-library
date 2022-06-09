59c219ea9177b6879816e29483881d289a9b465c
Kenji <terradoflex@gmail.com>
2022-06-10 05:37:15 +0800

    v1.0.2.2 - home inputs

--- theme/views/home.php
+++ theme/views/home.php
@@ -1,14 +1,15 @@
 <div class="fx jc">
     <div class="content-medium">
         <div class="move-on-keyup">
-            <div class="pop text-lg">The Mindanao Project.</div>
+            <div class="pop text-xl site-home-title">The Mindanao Project.</div>
         </div>
-        <input id="home-search-box" class="wf" type="text" placeholder="Search for regions, provinces, cities and towns">
+        <div class="space-lg"></div>
+        <fieldset class="border-rd-md">
+            <input id="home-search-box" class="wf no-border input-md int text-md" type="text" placeholder="Search for regions, provinces, cities and towns">
+        </fieldset>
+
         <div id="home-search-results">
 
         </div>
     </div>
 </div>
-<style media="screen">
-    .move-on-keyup {margin-top:45%; transition: 0.3s;}
-</style>
